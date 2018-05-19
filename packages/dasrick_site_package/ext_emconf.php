d70758b03c2d8b1b7317d3e042144cabb1a6ffed
Enrico Hoffmann <dasrick@gmail.com>
2018-05-19 22:11:42 +0200

    replace original package of helhum (thx to helmut)

--- packages/dasrick_site_package/ext_emconf.php
+++ packages/dasrick_site_package/ext_emconf.php
@@ -1,17 +1,17 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-  'title' => 'Helhum TYPO3 Distribution Site Package',
-  'description' => 'An example site package for Helhum TYPO3 Distribution',
+  'title' => 'dasrick TYPO3 Distribution Site Package',
+  'description' => 'An example site package for dasrick TYPO3 Distribution',
   'category' => 'TYPO3 Console',
   'state' => 'stable',
   'uploadfolder' => 0,
   'createDirs' => '',
   'modify_tables' => '',
   'clearCacheOnLoad' => 0,
-  'author' => 'Helmut Hummel',
-  'author_email' => 'info@helhum.io',
-  'author_company' => 'helhum.io',
-  'version' => '0.1.0',
+  'author' => 'Enrico Hoffmann',
+  'author_email' => 'dasrick@gmail.com',
+  'author_company' => 'dasrick.de',
+  'version' => '0.0.0',
   'constraints' => [
     'depends' => [
       'typo3' => '8.7.0-8.7.99',
