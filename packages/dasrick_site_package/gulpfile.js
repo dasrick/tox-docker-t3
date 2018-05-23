419bb1201507e14e62dea6d1650bf797fa97f80c
Enrico Hoffmann <dasrick@gmail.com>
2018-05-23 20:19:16 +0200

    add multiple fonts/css libs and improve templates

--- packages/dasrick_site_package/gulpfile.js
+++ packages/dasrick_site_package/gulpfile.js
@@ -8,10 +8,30 @@ gulp.task('img', function () {
     .pipe(gulp.dest('Resources/Public'))
 });
 
-gulp.task('copy-bootstrap', function () {
+gulp.task('copy-dasrick-bootstrap', function () {
   return gulp
     .src('./node_modules/dasrick-bootstrap/dist/**/*.{css,js,map}')
     .pipe(gulp.dest('Resources/Public/Libs/dasrick-bootstrap'));
 });
+gulp.task('copy-dasrick-styleguide', function () {
+  return gulp
+    .src('./node_modules/dasrick-styleguide/public/css/style.css')
+    .pipe(gulp.dest('Resources/Public/Libs/dasrick-styleguide'));
+});
+gulp.task('copy-open-sans', function () {
+  return gulp
+    .src('./node_modules/npm-font-open-sans/**/*.{css,eot,svg,ttf,woff,woff2}')
+    .pipe(gulp.dest('Resources/Public/Libs/npm-font-open-sans'));
+});
+gulp.task('copy-open-sans-condensed', function () {
+  return gulp
+    .src('./node_modules/npm-font-open-sans-condensed/**/*.{css,eot,svg,ttf,woff,woff2}')
+    .pipe(gulp.dest('Resources/Public/Libs/npm-font-open-sans-condensed'));
+});
+gulp.task('copy-font-awesome', function () {
+  return gulp
+    .src('./node_modules/font-awesome/**/*.{css,map,eot,svg,ttf,woff,woff2}')
+    .pipe(gulp.dest('Resources/Public/Libs/font-awesome'));
+});
 
-gulp.task('default', ['img', 'copy-bootstrap']);
+gulp.task('default', ['img', 'copy-dasrick-bootstrap', 'copy-dasrick-styleguide', 'copy-open-sans', 'copy-open-sans-condensed', 'copy-font-awesome']);
